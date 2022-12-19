fc4405473a74130535c5e4251eb8069e8713c363
Mac Nyekan <macnyekan2@gmail.com>
2022-12-18 19:46:41 -0500

    created new files

---
+++ main.tf
@@ -0,0 +1,16 @@
+
+#configure aws provider
+provider "aws" {
+  region  = "us-east-1"
+  profile = "MacNyekan"
+}
+
+# Stores terraform state file in s3
+terraform {
+  backend "s3" {
+    bucket  = "macnyekan-terraform-state-file"
+    key     = "terraform.tfstate.dev"
+    region  = "us-east-1"
+    profile = "MacNyekan"
+  }
+}
